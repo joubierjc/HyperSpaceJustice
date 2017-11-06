ccfd61459dfd2467fda09177f25936628351a476
joubierjc <joubier.j.c@gmail.com>
2017-11-06 10:24:30 +0100

    Creating Hazard

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Boundaries") {
+            return;
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
