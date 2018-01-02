1883f2a8e4dbafbbc8705c74f993459146096774
joubierjc <joubier.j.c@gmail.com>
2018-01-02 23:57:37 +0100

    Added rigidbodies, so OnTriggerExit can work
    Also added a non-looking-good background (wip)

---
+++ Assets/Scripts/DestroyByBoundaries.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class DestroyByBoundaries : MonoBehaviour {
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Boundaries")) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ResetByBoundaries.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ResetByBoundaries : MonoBehaviour {
+
+    private Vector3 startingPosition;
+
+	private void Start () {
+        startingPosition = transform.position;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Boundaries")) {
+            transform.position = startingPosition; 
+        }
+    }
+
+}
