7e0cffa668f009126cbac3395c9e8558bcc65814
joubierjc <joubier.j.c@gmail.com>
2018-11-14 22:02:09 +0100

    new player controler

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 25f;
+
+	private Rigidbody rb;
+
+	private void Awake() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate() {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		rb.velocity = new Vector3(horizontal, vertical).normalized * speed;
+	}
+
+}
